21ec5e4b0fd0026d3ede6413422fe1d39c093717
aleXXXey11F <zanybeats@yandex.kz>
2025-09-26 10:13:19 +0300

    А

--- src/lab02/arrays.py
+++ src/lab02/arrays.py
@@ -1,4 +1,4 @@
-nums=[1.0, 1, 2.5, 2.5, 0]
+nums=[[1, 2], "ab"]
 def min_max(nums: list[float | int]) -> tuple[float | int, float | int]:
     if not nums:
         raise ValueError("Список не может быть пустым")
@@ -9,6 +9,17 @@ def unique_sorted(nums: list[float | int]) -> list[float | int]:
     nums_un=list(dict_nums)
     nums_un.sort()
     return nums_un
+def flatten(mat: list[list | tuple]) -> list:
+    result = []
+    for row in mat:
+        if not isinstance(row, (list, tuple)):
+            raise TypeError("Ожидался список или кортеж")
+        result.extend(row)
+    return result
+try:
+    print(flatten(nums))
+except TypeError as a:
+    print(f"Ошибка: {a}")
 print(unique_sorted(nums))
 try:    
     min,max= min_max(nums)  

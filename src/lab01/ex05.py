da28f6c62c07e2230bd92f4786208f2782e029b0
aleXXXey11F <zanybeats@yandex.kz>
2025-10-03 15:58:53 +0300

    5

--- src/lab01/ex05.py
+++ src/lab01/ex05.py
@@ -1,7 +1,7 @@
-fio_input = input().strip()
+fio_input = input("ФИО: ").strip() 
 fio_parts = fio_input.split()
 initials = ''.join([part[0].upper() for part in fio_parts if part])
-length = len(fio_input)
+length = len(fio_parts[0])+len(fio_parts[1])+len(fio_parts[2])
 
 print(f"Инициалы: {initials}.")
 print(f"Длина (символов): {length}.")

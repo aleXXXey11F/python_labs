f0e790e76b099b0ccedb0ddfb1de434310dd5240
aleXXXey11F <zanybeats@yandex.kz>
2025-10-16 23:52:03 +0300

    дфи3

--- src/lab03/lib/text.py
+++ src/lab03/lib/text.py
@@ -39,31 +39,30 @@ def top_n(freq: Dict[str, int], n: int = 5) -> List[Tuple[str, int]]:
     items = list(freq.items())
     sorted_items = sorted(items, key=lambda x: (-x[1], x[0]))
     return sorted_items[:n]
-"""
+
 # Тесты для normalize
 print("=== normalize ===")
-print(repr(normalize("ПрИвЕт\nМИр\t")))  # "привет мир"
-print(repr(normalize("ёжик, Ёлка", yo2e=True)))  # "ежик, елка"
-print(repr(normalize("Hello\r\nWorld")))  # "hello world"
-print(repr(normalize("  двойные   пробелы  ")))  # "двойные пробелы"
+print(repr(normalize("ПрИвЕт\nМИр\t")))  
+print(repr(normalize("ёжик, Ёлка", yo2e=True)))  
+print(repr(normalize("Hello\r\nWorld")))  
+print(repr(normalize("  двойные   пробелы  ")))  
 
 # Тесты для tokenize
 print("\n=== tokenize ===")
-print(tokenize("привет мир"))  # ["привет", "мир"]
-print(tokenize("hello,world!!!"))  # ["hello", "world"]
-print(tokenize("по-настоящему круто"))  # ["по-настоящему", "круто"]
-print(tokenize("2025 год"))  # ["2025", "год"]
-print(tokenize("emoji 😀 не слово"))  # ["emoji", "не", "слово"]
+print(tokenize("привет мир"))  
+print(tokenize("hello,world!!!"))  
+print(tokenize("по-настоящему круто"))  
+print(tokenize("2025 год"))  
+print(tokenize("emoji 😀 не слово"))  
 
 # Тесты для count_freq + top_n
 print("\n=== count_freq + top_n ===")
 tokens1 = ["a", "b", "a", "c", "b", "a"]
 freq1 = count_freq(tokens1)
-print(freq1)  # {"a": 3, "b": 2, "c": 1}
-print(top_n(freq1, 2))  # [("a", 3), ("b", 2)]
+print(freq1)  
+print(top_n(freq1, 2))  
 
 tokens2 = ["bb", "aa", "bb", "aa", "cc"]
 freq2 = count_freq(tokens2)
-print(freq2)  # {"aa": 2, "bb": 2, "cc": 1}
-print(top_n(freq2, 2))  # [("aa", 2), ("bb", 2)]
-"""
+print(freq2)  
+print(top_n(freq2, 2))  
